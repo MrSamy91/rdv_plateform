3a30a9b9b078f7289800ad2b1aeee3f7d664ede1
MrSamy91 <146086150+MrSamy91@users.noreply.github.com>
2024-10-02 14:13:50 +0200

    Delete monappli directory

--- monappli/create_db.py
+++
@@ -1,6 +0,0 @@
-from app import app, db  # Assure-toi d'importer aussi l'instance de l'application
-
-# Créer un contexte d'application
-with app.app_context():
-    db.create_all()  # Crée la base de données et les tables
-    print("Base de données initialisée !")
